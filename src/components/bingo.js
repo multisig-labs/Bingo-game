aebf80c6fb5913f74f02f7bc392fe0a7dea21fd1
JamieKessler <jkessler5535@gmail.com>
2024-10-02 18:35:27 -0700

    created free tile modal

--- src/components/bingo.js
+++ src/components/bingo.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import '../index.css';
 import Modal from './bingo-alert';
 import  { Link } from 'react-router-dom';
+import FreeModal from './free-modal';
 
 
 const generateBingoCard = () => {
@@ -26,13 +27,20 @@ const Bingo = () => {
   const [bingoCard, setBingoCard] = useState(generateBingoCard());
   const [clickedTiles, setClickedTiles] = useState(Array(3).fill(null).map(() => Array(3).fill(false)));
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isFreeModalOpen, setIsFreeModalOpen] = useState(false);
 
   const handleTileClick = (rowIndex, colIndex) => {
     const newClickedTiles = clickedTiles.map((row, rldx) =>
       row.map((clicked, cldx) => (rldx === rowIndex && cldx === colIndex ? !clicked : clicked))
     );
     setClickedTiles(newClickedTiles);
+
+    if (rowIndex === 1 && colIndex === 1) {
+      //If the Free tile is clicked, show free Modal
+      setIsFreeModalOpen(true);
+    } else {
     checkBingo(newClickedTiles);
+    }
   };
 
   const checkBingo = (tiles) => {
@@ -47,14 +55,16 @@ const Bingo = () => {
     setBingoCard(newBingoCard);
     setClickedTiles(Array(3).fill(null).map(() => Array(3).fill(false)));
     setIsModalOpen(false);
+    setIsFreeModalOpen(false);
   };
 
   return (
-    <div className="bingo container">
-      <h1 className="bingo-title">Bingo</h1>
+    <div className={`bingo container ${isModalOpen || isFreeModalOpen ? 'blur' : ''}`}>
       <Link to="/" className="link" >
         How to win?
       </Link>
+      <h2>GoGopool and Uplink present</h2>
+      <h1 className="bingo-title">Bingo</h1>
       <div className="bingo-grid">
         {bingoCard.map((row, rowIndex) =>
           row.map((_, colIndex) => {
@@ -90,6 +100,8 @@ const Bingo = () => {
       </button>
       
       <Modal isOpen={isModalOpen} onClose={resetGame}/>
+      <FreeModal isOpen={isFreeModalOpen} onClose={() => 
+        setIsFreeModalOpen(false)} />
     </div>
   );
 };
---
+++ src/components/free-modal.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import '../index.css';
+
+const FreeModal = ({ isOpen, onClose}) => {
+  if (!isOpen) return null;
+
+  const openTwitter = () => {
+    window.open('https://twitter.com/yourtwitterhandle', '_blank');
+  }
+
+  return (
+    <div className="modal-overlay" onclick={onClose}>
+      <div className="modal-content" onClick={(e) => e.stopPropagation()}>
+        <h2>Free Square.</h2>
+        <p>Post a fun tweet on X and mention @GoGoPool_.
+          Be sure to show your tweet to Breevie if you get Bingo!
+        </p>
+        <button className="dont-post" onClick={onClose}>I don't want to post</button>
+        <button onClick={openTwitter}>Post Now!</button>
+      </div>
+    </div>
+  );
+};
+
+export default FreeModal;
