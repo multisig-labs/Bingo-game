26acd035c0ed5fdf1a13a2649670ea0b0bcfa84f
JamieKessler <jkessler5535@gmail.com>
2024-10-08 14:22:46 -0700

    changed width

--- src/components/game-directions.js
+++ src/components/game-directions.js
@@ -7,6 +7,7 @@ import '../index.css';
 import HeaderImg from '../assets/images/header-img';
 import BingoIcon from '../assets/images/bingo-logo';
 import Footer from '../assets/images/sponsors-img';
+import Sponsors from '../assets/images/sponsors-img';
 
 
 const GameDirections = () => {
@@ -26,15 +27,17 @@ const GameDirections = () => {
       
 
       <div className="directions">
-        <h3 className="sub-heading"> Here's how to win</h3>
-        <p>Go around the room to network with people. When you meet someone on the list, take a self with them for verfication proof. The first person to complete a row, column, or diagnol of squares of people will be in Bingo mode. Find Breevie immediately, show him the selfies, and claim your prize. Until all the criteria is met, the game will go on.</p>
-        <p>Don’t forget and good luck!</p>
+          <h3 className="how-to-win"> Here's how to win</h3>
+        <div className="inner-directions">
+          <p>Go around the room to network with people. When you meet someone on the list, take a self with them for verfication proof. The first person to complete a row, column, or diagnol of squares of people will be in Bingo mode. Find Breevie immediately, show him the selfies, and claim your prize. Until all the criteria is met, the game will go on.</p>
+          <p><b>Don’t forget and good luck!</b></p>
+        </div>
 
         <button className="accept-btn" onClick={handleAcceptRules}>
           I understand, and I'm ready to play!
         </button>
-        <Footer/>
       </div>
+      <Sponsors/>
     </div>
   )
 };
