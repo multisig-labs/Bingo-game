c6fe62b4157ff310ee4b7f064148e1ac24c3d6f0
JamieKessler <jkessler5535@gmail.com>
2024-10-08 17:50:06 -0700

    started styling on free tile modal

--- src/components/free-modal.js
+++ src/components/free-modal.js
@@ -11,12 +11,12 @@ const FreeModal = ({ isOpen, onClose}) => {
   return (
     <div className="modal-overlay" onClick={onClose}>
       <div className="modal-content" onClick={(e) => e.stopPropagation()}>
-        <h2>Free Square</h2>
+        <h1 className="free-square">Free Square</h1>
         <p>Post a fun tweet on X and mention @GoGoPool_.
           Be sure to show your tweet to Breevie if you get Bingo!
         </p>
-        <button className="dont-post" onClick={onClose}>I don't want to post</button>
-        <button onClick={openTwitter}>Post Now!</button>
+        <button className="dont-post-btn" onClick={onClose}>I don't want to post</button>
+        <button className="open-twitter-btn" onClick={openTwitter}>Post Now!</button>
       </div>
     </div>
   );
@@ -37,8 +37,8 @@ return (
       <p>Post a fun tweet on X and mention @GoGoPool_.
         Be sure to show your tweet to Breevie if you get Bingo!
       </p>
-      <button className="dont-post" onClick={onClose}>I don't want to post</button>
-      <button onClick={openTwitter}>Post Now!</button>
+      <button className="dont-post" onClick={onClose}>I Don't Want<span className="no-post-span">To Post</span></button>
+      <button className="post-now" onClick={openTwitter}>Post Now!</button>
     </div>
   </div>
 );
